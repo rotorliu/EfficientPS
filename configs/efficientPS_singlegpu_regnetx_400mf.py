5fef00fa09807f98ad8f408884d399664ed9da4d
NAVINFO\liubao <liubao@navinfo.com>
2021-08-27 11:21:00 +0800

    support regnet

--- configs/efficientPS_singlegpu_regnetx_400mf.py
+++ configs/efficientPS_singlegpu_regnetx_400mf.py
@@ -1,18 +1,15 @@
 # model settings
 model = dict(
     type='EfficientPS',
-    pretrained=True,
+    pretrained='open-mmlab://regnetx_400mf',
     backbone=dict(
-        _delete_=True,
         type='RegNet',
-        arch='regnetx_400mf',
+        arch='regnetx_8.0gf',
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
-        style='pytorch',
-        init_cfg=dict(
-            type='Pretrained', checkpoint='open-mmlab://regnetx_400mf')),
+        style='pytorch'),
     neck=dict(
         type='FPN',
         in_channels=[32, 64, 160, 384],
