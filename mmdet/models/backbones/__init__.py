f049a3db4fc3c533fd36fb4e43749a11b329537e
NAVINFO\liubao <liubao@navinfo.com>
2021-08-27 11:05:53 +0800

    support regnet

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,3 +1,5 @@
+from .regnet import RegNet
 from .resnet import ResNet, make_res_layer
+from .resnext import ResNeXt
 
-__all__ = ['ResNet', 'make_res_layer']
+__all__ = ['RegNet', 'ResNet', 'make_res_layer', 'ResNeXt']
